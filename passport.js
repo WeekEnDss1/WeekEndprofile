44c09b3c48651c92971443af6694c295dbd9a3c2
AnAs Rz <63547588+anasrz@users.noreply.github.com>
2023-06-07 21:59:06 +0300

    Update passport.js

--- passport.js
+++ passport.js
@@ -5,8 +5,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 module.exports = (passport) => {
   passport.use(
     new GoogleStrategy({
-      clientID: '897974452598-jat413l8f6f03ueol5loe75pgg7bjjb3.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientID: '',
+      clientSecret: '',
       callbackURL: `https://anasdev.site/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => done(null, profile)
